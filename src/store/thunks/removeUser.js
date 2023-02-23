17a2bda31085a89e6ad1e8882ca492374b69eea0
Mohamad Noruzi <113915687+mohamadNoruzi@users.noreply.github.com>
2023-02-23 14:05:47 +0330

    Update removeUser.js

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const removeUser = createAsyncThunk("users/remove", async (user) => {
-  await axios.delete(`http://localhost:3005/users/${user.id}`);
+  await axios.delete(`https://seen-fluoridated-branch.glitch.me/users/${user.id}`);
 
   return user;
 });
