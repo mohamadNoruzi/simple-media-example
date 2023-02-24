258831a739833fe5525779999a45071ff9d9833f
Mohamad Noruzi <113915687+mohamadNoruzi@users.noreply.github.com>
2023-02-24 11:27:19 +0330

    Update fetchUsers.js

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("https://seen-fluoridated-branch.glitch.me/users");
+  const response = await axios.get("https://cliff-immediate-alto.glitch.me/users");
 
   return response.data;
 });
