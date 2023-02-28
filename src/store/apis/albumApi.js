9bd46395317cb190cab56a60ba1453812dfc8059
Mohamad Noruzi <113915687+mohamadNoruzi@users.noreply.github.com>
2023-02-28 18:44:17 +0330

    Update albumApi.js

--- src/store/apis/albumApi.js
+++ src/store/apis/albumApi.js
@@ -4,7 +4,7 @@ import { faker } from "@faker-js/faker";
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3005",
+    baseUrl: "https://aquatic-lean-work.glitch.me",
     fetchFn: async (...args) => {
       // REMOVE FOR R=PRODUCTION
       await pause(1000);
