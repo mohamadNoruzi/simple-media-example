d82de453030156ea6285bbfdab1348e1a6ec2b6f
Mohamad Noruzi <113915687+mohamadNoruzi@users.noreply.github.com>
2023-02-28 18:45:18 +0330

    Update fetchUsers.js

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("http://localhost:3005/users");
+  const response = await axios.get("https://aquatic-lean-work.glitch.me/users");
 
   return response.data;
 });
