083efd5df29237e71175cb2856ba13ba55d347a4
Mohamad Noruzi <113915687+mohamadNoruzi@users.noreply.github.com>
2023-02-28 16:40:43 +0330

    Update photosApi.js

--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -4,7 +4,7 @@ import { faker } from "@faker-js/faker";
 const photosApi = createApi({
   reducerPath: "photos",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://cliff-immediate-alto.glitch.me",
+    baseUrl: "http://localhost:3005",
   }),
   endpoints(builder) {
     return {
